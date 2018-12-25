1f8ffe7dbcd820dce03c6c2dcab4821bb8c51869
ChristianVisintin <christian.visintin1997@gmail.com>
2018-12-25 17:09:03 +0100

    Added strutils to murmure

---
+++ src/utils/strutils.cpp
@@ -0,0 +1,35 @@
+/**
+ *  Murmure - Net-SNMP MIB Versatile Extender
+ *  Developed by Christian Visintin
+ * 
+ * 	Copyright (C) 2018 - 2019 Christian Visintin
+ *
+ *	This file is part of "Murmure"
+ *
+ *  Murmure is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  Murmure is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+**/
+
+#include <utils/strutils.hpp>
+
+#include <sstream>
+
+using namespace strutils;
+
+std::vector<std::string> split(const std::string& s, char delimiter) {
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
