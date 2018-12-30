be087431a3e632f4c99ea30cf4076a1fa3268662
ChristianVisintin <christian.visintin1997@gmail.com>
2018-12-30 11:20:38 +0100

    Implementation of modulefacade.cpp

---
+++ src/murmure/modulefacade.cpp
@@ -0,0 +1,134 @@
+/**
+ *  Murmure - Net-SNMP MIB Versatile Extender
+ *  Developed by Christian Visintin
+ * 
+ * 	Copyright (C) 2018 - 2019 Christian Visintin
+ *
+ *	This file is part of "Murmure"
+ *
+ *  Murmure is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  Murmure is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+**/
+
+#include <murmure/modulefacade.hpp>
+
+#include <algorithm>
+
+using namespace murmure;
+
+/**
+ * @function ModuleFacade
+ * @description ModuleFacade class constructor
+**/
+
+ModuleFacade::ModuleFacade() {
+  module = nullptr;
+  //If modules hasn't been loaded yet, load modules
+  if (!modulesLoaded) {
+    loadModules();
+  }
+}
+
+/**
+ * @function ~ModuleFacade
+ * @description ModuleFacade class destructor
+**/
+
+ModuleFacade::~ModuleFacade() {
+  //Delete module if exists
+  if (module != nullptr) {
+    delete module;
+  }
+}
+
+/**
+ * @function findModule
+ * @description starting from the typename read from MIB, check if module exists. If exists instance new Module
+ * @param std::string typeName
+ * @returns bool: true if module has been found and successfully initialized
+**/
+
+bool ModuleFacade::findModule(std::string typeName) {
+
+  //Check if typename is in module list
+  std::map<std::string, Module* (*)()>::iterator moduleIterator;
+  for (moduleIterator = modules.begin(); moduleIterator != modules.end(); moduleIterator++) {
+    if (typeName == moduleIterator->first) {
+      //Module found
+      module = modules[typeName]();
+      return true;
+    }
+  }
+  //Module not found
+  return false;
+}
+
+/**
+ * @function setValue
+ * @description set value to module instance
+ * @param std::string oid
+ * @param std::string value
+ * @returns bool: true if value was successfully set
+**/
+
+bool ModuleFacade::setValue(std::string oid, std::string value) {
+  if (module == nullptr) {
+    return false;
+  }
+  return module->setValue(oid, value);
+}
+
+/**
+ * @function getPrintableValue
+ * @description get printable value from module instance
+ * @returns std::string printable value
+**/
+
+std::string ModuleFacade::getPrintableValue() {
+  if (module == nullptr) {
+    return "";
+  }
+  return module->getPrintableValue();
+}
+
+/**
+ * @function getPrimitiveType
+ * @description returns the primitive type name associated to module
+ * @returns std::string
+**/
+
+std::string ModuleFacade::getPrimitiveType() {
+  if (module == nullptr) {
+    return "";
+  }
+  return module->getPrimitiveType();
+}
+
+/**
+ * @function loadModules
+ * @description load modules into 'modules' hash map
+ * NOTE: this function is @!static
+ * NOTE: map["ModuleClass"] = &createInstance<ModuleClass>;
+ * NOTE: part of this function identified by 'AUTO' is automatically generated during compilation
+**/
+
+void ModuleFacade::loadModules() {
+
+  //Clear map
+  modules.clear();
+
+  //Load modules into hash map
+  /* @!::AUTO::@! */
+
+  //Set modules loaded to true
+  modulesLoaded = true;
+  return;
+}
